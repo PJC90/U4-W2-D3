7d57c89cecffce8196ddb29cee152c9cdb7fe4c2
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-12-06 16:58:09 +0100

    es3 -es4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import entities.Customer;
+import entities.Order;
 import entities.Product;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +19,10 @@ public class Main {
         products.add(new Product("Seneca", "Book",12.4));
         products.add(new Product("Homo-Deus", "Book",19.7));
 
+        List<Order> carrello = new ArrayList<>();
+        carrello.add(new Order("ok", LocalDate.of(2022,2,15),LocalDate.of(2022,5,15),products,new Customer("Filippo",2)));
+        carrello.add(new Order("ok", LocalDate.of(2022,2,15),LocalDate.of(2022,5,15),products,new Customer("Gigi",3)));
+
         products.forEach(product -> System.out.print(product.getName() + ", "));
         System.out.println();
         System.out.println("*****   Prezzi maggiori di 100€   ******");
@@ -24,5 +31,15 @@ public class Main {
 //        products.stream().filter(product -> "Baby".equals(product.getCategory())).forEach(product -> System.out.println(product.getName()));
         List<String> baby = products.stream().filter(product -> "Baby".equals(product.getCategory())).map(product -> product.getName()).toList();
         baby.forEach(bab -> System.out.println(bab));
+        System.out.println("*****  Catogoria Boys 10% discount  *****");
+
+        List<String> boys = products.stream().filter(product -> "Boys".equals(product.getCategory()))
+                .map(product -> (product.getName() + " " + (product.getPrice() - product.getPrice()*0.10))).toList();
+        //        boys.forEach(boy -> System.out.println(boy));
+        boys.forEach(System.out::println);
+
+        System.out.println("************  Carrello  **************");
+        carrello.forEach(car -> System.out.println(car.getCustomer().getName()));
+
     }
 }
